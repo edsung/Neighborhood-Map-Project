a7b47ec41c8fd66ff5feab95e64c00e294362851
Edward Sung <songchengyun@gmail.com>
2016-06-19 18:38:53 -0400

    project 5 submission version

--- js/app.js
+++ js/app.js
@@ -17,49 +17,50 @@ var sites = [{
   	     lng: -84.3650,
          title: 'Historic Fourth Ward Park'	
   	     },{
-  	     lat: 33.7707, 
-  	     lng: -84.3644,
-         title: 'The Masquerade'	
+  	     lat: 33.7729, 
+  	     lng: -84.3657,
+         title: 'Ponce City Market'	
   	     }];
 
+//Setup ko.observable data structure.
 var atlLocations = function (data) {
   this.title = ko.observable(data.title);
   this.lat = ko.observable(data.lat);
   this.lng = ko.observable(data.lng);
 };
 
-var wikiLink=[];
+//Global variable to store the response from the ajax request.
 var wikiTxt="";
-var text ="<p>%st</p>";
 
 function initMap() {
 
+      //creates google map object with center at old fourth ward atlanta.
       var map = new google.maps.Map(document.getElementById('map'), {
          center: {lat: 33.7635, lng: -84.3644},
          zoom: 15
         });
 
+      //calling ko view model.
       ko.applyBindings(new ViewModel(map));
 }
   
 function ViewModel(map) {
         
       var self = this;
-
-        //add locations to list.
       this.searchKey = ko.observable('');
     	this.locations = ko.observableArray();
-    	this.infowindow ; //= ko.observableArray();
+    	this.infowindow ;
       this.markers;
     	this.fnd = ko.observable();
 
+      //inserting each site in an observable array
       sites.forEach(function (data) {
           self.locations.push(new atlLocations(data));
       });
 
-      function wikiFetch(locations, site){
-        var wikiUrl = 'http://en.wikipedia.org/w/api.php?&format=json&action=query&prop=extracts&exintro&explaintext&titles=' + site+ '&redirects=1&indexpageids=0';
-        //var url='hello';
+      //ajax call to wikipedia to get intro article for each site.
+      function wikiFetch(locations, site, marker){
+        var wikiUrl = 'http://en.wikipedia.org/w/api.php?&format=json&action=query&prop=extracts&exintro&explaintext&titles=' + site+ '&redirects=2&indexpageids=0';
 
         $.ajax({
           url: wikiUrl,
@@ -67,23 +68,33 @@ function ViewModel(map) {
           success: function(response) {
                       var contentIdx = response.query.pageids[0];
                       wikiTxt = response.query.pages[contentIdx].extract;
-                      locations.info = ko.observable(wikiTxt);
-                      wikiLink.push(wikiTxt); 
-                      //response.query.pages[contentIdx].extract);                      
-                      //self.locations()[i].content = response.query.pages[contentIdx].extract;
+                      locations.info = wikiTxt;
+
+                      google.maps.event.addListener(marker,'click', (function(marker){
+                      return function() {
+                      map.panTo(marker.getPosition());
+                      infowindow.setContent(locations.info);
+                      infowindow.open(map, marker);
+
+                      // Google Maps API marker animation
+                      marker.setAnimation(google.maps.Animation.BOUNCE);
+                      setTimeout(function(){ marker.setAnimation(null); }, 900);
+                       };
+                      })(marker));
                   },
 
           error: function (response, textStatus, errorThrown) {
-                  wikiLink.push('ERROR RETRIEVING ARTICLE '+ errorThrown);
                   }
         });
 
       }
 
+      //create infoWindow object.
       infowindow = new google.maps.InfoWindow({
                       content: ''
         });
  
+      //setting the marker and infowindow content for each site.
     	for(i=0;i<sites.length;i++){
 
       
@@ -93,16 +104,12 @@ function ViewModel(map) {
           title:sites[i].title
   	     		});
 
-        wikiFetch(self.locations()[i], sites[i].title);
+        wikiFetch(self.locations()[i], sites[i].title, marker);
 
-        var holder = self.locations()[i];
-
-        console.log(holder);
-
-    		google.maps.event.addListener(marker,'click', (function(marker, holder){
+    		google.maps.event.addListener(marker,'click', (function(marker){
     			return function() {
                 map.panTo(marker.getPosition());
-                infowindow.setContent();
+
                 infowindow.open(map, marker);
 
               	// Google Maps API marker animation
@@ -111,59 +118,33 @@ function ViewModel(map) {
             	};
     		})(marker));
 
+        //load marker object of each site into observable array. Used for enable and disable visibility on map.
     		self.locations()[i].markers = marker;
 
     	}//END FOR loop
 
+      //function to set visible that are not filtered from the search textbox.
       function clearMarker(){
-          //for(x = 0; x<self.locations().length;x++){
           ko.utils.arrayForEach(self.locations(), function(location){
 
                 location.markers.setVisible(location.title().toLowerCase().includes(self.searchKey().toLowerCase()));
-
-                //if(location.title().toLowerCase().includes(self.searchKey().toLowerCase())){
-                  //google.maps.event.trigger(location.markers, 'click');
-                //}
-          //}
           });
       }
 
-       self.toggleBounce = function(location) {
-
-        //ko.utils.arrayForEach(self.locations(), function(location){
-        //if (location.markers.getAnimation() !== null) {
-        //      location.markers.setAnimation(null);
-        //} else {
-        google.maps.event.trigger(location.markers, 'click');
-
-        
+      //function that bounces the marker when user clicks the name in the list.
+      self.toggleBounce = function(location) {
+        google.maps.event.trigger(location.markers, 'click');        
       }
 
+      //This function is the search filter.
       self.search = ko.computed(function(){
           var index = ko.observableArray([]);
-          //console.log();
-          //self.locations()[i].title().toLowerCase().includes(self.searchKey().toLowerCase())== true)
-          //return ko.utils.arrayFilter(self.locations(), function(){
-          //return self.locations()[0].title().toLowerCase().includes(self.searchKey().toLowerCase())
-          //locations().forEach(function())
-
-          //The following three line performs filtering.
           return ko.utils.arrayFilter(self.locations(), function(location){    
 
               clearMarker();
               return (location.title().toLowerCase().includes(self.searchKey().toLowerCase()));;
           });
 
-          //return ko.utils.arrayFilter(self.locations(), function(location){
-
-            //return location.title().toLowerCase().includes(self.searchKey().toLowerCase());
-
-          //});   
-              
-        
-          //self.locations.title.toLowerCase.indexOf(self.searchKey);
-          //console.log(self.locations().indexOf(self.searchKey().toLowerCase()));
-          //});
-
       });
+
 } 
